842943f41d2a2a63754a838b5291c93355680a27
panamabilly <billsweeney85@gmail.com>
2022-02-15 17:45:54 -0600

    Add POST and GET ALL routes for Pins

---
+++ controllers/pinController.js
@@ -0,0 +1,35 @@
+// Require the Express module
+const express = require('express');
+
+// Instantiate a router -- this will hold all the logic
+// for the URLs + methods for this resource
+const router = express.Router();
+
+// Import the Resorts model
+const Pin = require('../models/Pin');
+// Create a pin
+router.post('/', async (req, res) => {
+	const newPin = new Pin(req.body);
+	try {
+		const savePin = await newPin.save();
+		res.status(201).json(savePin);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// Get all pins
+
+router.get('/', async (req, res) => {
+	try {
+		// 1. Get all of the bookmarks from the DB
+		const pins = await Pin.find({});
+		// 2. Send them back to the client as JSON
+		res.status(200).json(pins);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+// Export this router object so that it is accessible when we require the file elsewhere
+
+module.exports = router;
