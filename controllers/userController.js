e436eb8dfafcb0c2df84ca15b6488d40c8ac9735
panamabilly <billsweeney85@gmail.com>
2022-02-15 18:50:05 -0600

    Update usercontroller with POST path that hashes password with bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,20 +35,27 @@ router.get('/', async (req, res) => {
 module.exports = router;
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-//register
+//register and using bcrypt to hash password
 router.post('/register', async (req, res) => {
 	try {
 		//generate new password
-
-		//create new user 
-
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bycrpt.hash(req.body.password, salt);
+		//create new user
+		const newUser = new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: hashedPassword,
+		});
 		// save user and save response
-
-	} catch(err){
-		res.status(500).json(err)
+		const user = await new User.save();
+		res.status(200).json(user._id);
+	} catch (err) {
+		res.status(500).json(err);
 	}
-})
+});
 
 //login
 
